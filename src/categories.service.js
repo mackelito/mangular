ce0848653ccd7a4d12eae0632f1aac8c05f7dd7d
Markus Thiel <markus@thiels.se>
2016-04-18 21:35:54 +0200

    Adding product and category services

---
+++ src/categories.service.js
@@ -0,0 +1,36 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('mangular')
+    .service('Categories', Service);
+
+  Service.$inject = ['Restangular', '$log'];
+
+  /* @ngInject */
+  function Service(Restangular, $log) {
+    $log.info('--- Categories service start ---');
+
+    var service = {
+      getCategories: getCategories,
+      getCategory: getCategory
+    };
+
+    return service;
+
+    function getCategories() {
+      $log.info('--- Featching categories start ---');
+      $log.info('Featching categories:');
+      $log.info('--- Featching categories end ---');
+      return Restangular.all('categories').customGET();
+    }
+
+    function getCategory(id) {
+      $log.info('--- Featching category start ---');
+      $log.info('--- Featching category ' + id + ' ---');
+      $log.info('--- Featching category end ---');
+      return Restangular.one('category/' + id).customGET();
+    }
+
+  }
+})();
---
+++ src/products.service.js
@@ -0,0 +1,41 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('mangular')
+    .service('Products', Service);
+
+  Service.$inject = ['Restangular','$stateParams','$log'];
+
+  /* @ngInject */
+  function Service(Restangular,$stateParams, $log) {
+    $log.info('--- Products service start ---');
+
+    var service = {
+      getProducts: getProducts,
+      getProduct: getProduct
+    };
+
+    return service;
+
+    function getProducts(numberOfProducts, $stateParams) {
+      $log.info('--- Featching products start ---');
+      $log.info('Featching ' + numberOfProducts + ' products from ' + $stateParams);
+      // var categoryId = '/categories/' + $stateParams.categoryId;
+      $log.info('--- Featching products end ---');
+
+      return Restangular.all('/products?searchCriteria[page_size]=' + numberOfProducts).customGET();
+    }
+
+    function getProduct(sku) {
+      $log.info('--- Featching product start ---');
+      $log.info('Featching product ' + sku);
+      $log.info('--- Featching product end ---');
+
+      return Restangular.one('products/' + sku).customGET();
+    }
+
+    $log.info('--- Products service end ---');
+
+  }
+})();

685284e4c19515267443871848a527e92b226320
Markus Thiel <markus@thiels.se>
2016-08-26 09:56:01 +0200

    removed logging

--- src/categories.service.js
+++ src/categories.service.js
@@ -9,7 +9,6 @@
 
   /* @ngInject */
   function Service(Restangular, $log) {
-    $log.info('--- Categories service start ---');
 
     var service = {
       getCategories: getCategories,
@@ -19,22 +18,12 @@
     return service;
 
     function getCategories() {
-      $log.info('--- Featching categories start ---');
-      $log.info('--- Featching categories: ---');
-      $log.info('--- Featching categories end ---');
-
       return Restangular.all('categories').customGET();
     }
 
     function getCategory(id) {
-      $log.info('--- Featching category start ---');
-      $log.info('--- Featching category ' + id + ' ---');
-      $log.info('--- Featching category end ---');
-
       return Restangular.one('category/' + id).customGET();
     }
-
-    $log.info('--- Categories service end ---');
 
   }
 })();

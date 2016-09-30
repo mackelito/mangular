86c8d12779cb48db488b804a87cf80ced063fbcc
Oskar Thiel <oskar.thiel@vaimo.com>
2016-09-30 23:33:59 +0200

    Add caching to urlRewrite
    Update the cache on every page reload for now.

--- src/urlRewrite.service.js
+++ src/urlRewrite.service.js
@@ -1,18 +1,33 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('mangular')
-    .service('UrlRewrite', Service);
+    angular
+        .module('mangular')
+        .service('UrlRewrite', Service);
 
-  Service.$inject = ['Restangular'];
+    Service.$inject = ['Restangular', '$localForage', '$filter'];
 
-  /* @ngInject */
-  function Service(Restangular) {
-    return {
-      getUrlRewrite: function (path) {
-        return Restangular.one('url-rewrite/find-one-by-request-path/' + path).customGET();
-      }
-    };
-  }
+    /* @ngInject */
+    function Service(Restangular, $localForage, $filter) {
+        return {
+            getUrlRewrite: getUrlRewrite,
+            getUrlRewritesByTypes: getUrlRewritesByTypes
+        };
+
+
+        function getUrlRewrite(path) {
+            return $localForage.getItem('urlRewrites').then(function (urlRewrites) {
+                return $filter('filter')(urlRewrites, {
+                    request_path: path
+                })[0] || Restangular.one('url-rewrite/find-one-by-request-path/' + path).customGET();
+            });
+
+        }
+
+        function getUrlRewritesByTypes(types) {
+            return Restangular.one('url-rewrite/find-all-by-entity-type').customPOST({
+                entity_types: types
+            });
+        }
+    }
 })();

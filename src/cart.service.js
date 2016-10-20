b1e3a9e581d886e684060b5757df0af87c0f6e41
Markus Thiel <markus@thiels.se>
2016-10-20 10:03:27 +0200

    added placeOrder and getPaymentInformation

--- src/cart.service.js
+++ src/cart.service.js
@@ -1,89 +1,119 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('mangular')
-    .factory('Cart', Factory);
+    angular
+        .module('mangular')
+        .factory('Cart', Factory);
 
-  Factory.$inject = ['Restangular', '$log', '$localForage', '$rootScope'];
+    Factory.$inject = ['Restangular', '$log', '$localForage', '$rootScope'];
 
-  /* @ngInject */
-  function Factory(Restangular, $log, $localForage, $rootScope) {
+    /* @ngInject */
+    function Factory(Restangular, $log, $localForage, $rootScope) {
 
-    var service = {
-      getCartId: getCartId,
-      getTotals: getTotals,
-      getItems: getItems,
-      addItem: addItem,
-      removeItem: removeItem,
-      createNewCart: createNewCart
-    };
+        var service = {
+            getCartId: getCartId,
+            getTotals: getTotals,
+            getItems: getItems,
+            addItem: addItem,
+            removeItem: removeItem,
+            createNewCart: createNewCart,
+            getPaymentInformation: getPaymentInformation,
+            placeOrder: placeOrder
+        };
 
-    $localForage.getItem('cartId').then(function(id) {
-      service.cartId = id;
-    });
-
-    var cart = {};
-    $localForage.getItem('cartId').then(function(id) {
-      cart.id = id;
-      service.cartId = id;
-    });
-
-    return service;
-
-    function getCartId() {
-      return $localForage.getItem('cartId');
-    }
-
-    function getTotals() {
-      var getTotals = getCartId().then(function(id){
-        return Restangular.all('guest-carts/'+ id + '/totals').withHttpConfig({cache: false}).customGET();
-      });
-      return getTotals;
-    }
-
-    function getItems() {
-      var cartItems = getCartId().then(function(id){
-        return Restangular.all('guest-carts/'+ id + '/items').withHttpConfig({cache: false}).customGET();
-      });
-      return cartItems;
-    }
-
-    function addItem(product, cartId) {
-      $localForage.getItem('cartId').then(function(cartId) {
-        var data = {
-              'cartItem': {
-                'sku': product.sku,
-                'qty': 1,
-                'quote_id': cartId
-              }
-            };
-        Restangular.one('guest-carts').one(cartId).one('items').customPOST(data)
-        .then(function(response) {
-          $log.info('cartUpdated:');
-          $rootScope.$broadcast('cartUpdated', response);
+        $localForage.getItem('cartId').then(function(id) {
+            service.cartId = id;
         });
-      });
-    }
 
-    function removeItem(itemId) {
-      console.log(itemId);
-      var cartItems = getCartId().then(function(cartId){
-        return Restangular.all('guest-carts/'+ cartId + '/items/' + itemId).remove()
-        .then(function(response) {
-          $log.info('cartUpdated:');
-          $rootScope.$broadcast('cartUpdated', response);
+        var cart = {};
+        $localForage.getItem('cartId').then(function(id) {
+            cart.id = id;
+            service.cartId = id;
         });
-      });
-      // return cartItems;
-    }
 
-    function createNewCart() {
-      var createNewCart = Restangular.all('guest-carts');
-      var cartId = createNewCart.post();
-      return cartId;
-    }
+        return service;
 
-    $log.info('--- Cart service end ---');
-  }
+        function getCartId() {
+            return $localForage.getItem('cartId');
+        }
+
+        function getTotals() {
+            var getTotals = getCartId().then(function(id) {
+                return Restangular.all('guest-carts/' + id + '/totals').withHttpConfig({
+                    cache: false
+                }).customGET();
+            });
+            return getTotals;
+        }
+
+        function getItems() {
+            var cartItems = getCartId().then(function(id) {
+                return Restangular.all('guest-carts/' + id + '/items').withHttpConfig({
+                    cache: false
+                }).customGET();
+            });
+            return cartItems;
+        }
+
+        function addItem(product, cartId) {
+            $localForage.getItem('cartId').then(function(cartId) {
+                var data = {
+                    'cartItem': {
+                        'sku': product.sku,
+                        'qty': 1,
+                        'quote_id': cartId
+                    }
+                };
+                Restangular.one('guest-carts').one(cartId).one('items').customPOST(data)
+                    .then(function(response) {
+                        $log.info('cartUpdated:');
+                        $rootScope.$broadcast('cartUpdated', response);
+                    });
+            });
+        }
+
+        function removeItem(itemId) {
+            console.log(itemId);
+            var cartItems = getCartId().then(function(cartId) {
+                return Restangular.all('guest-carts/' + cartId + '/items/' + itemId).remove()
+                    .then(function(response) {
+                        $log.info('cartUpdated:');
+                        $rootScope.$broadcast('cartUpdated', response);
+                    });
+            });
+        }
+
+        function createNewCart() {
+            var createNewCart = Restangular.all('guest-carts');
+            var cartId = createNewCart.post();
+            return cartId;
+        }
+
+        function getPaymentInformation(cartId) {
+            var getPaymentInformation = getCartId().then(function(id) {
+                return Restangular.all('guest-carts/' + id + '/payment-information').customGET();
+            });
+            return getPaymentInformation;
+        }
+
+        // function setPaymentInformation(paymentInformation) {
+        //     var getPaymentInformation = getCartId().then(function(id) {
+        //         return Restangular.all('guest-carts/' + id + '/payment-information').customPOST(paymentInformation);
+        //     });
+        //     return getPaymentInformation;
+        // }
+
+        function placeOrder(cartId) {
+            var placeOrder = getCartId().then(function(id) {
+                cartId = id;
+                var orderData = getPaymentInformation().then(function(paymentinformation) {
+                    return Restangular.one('guest-carts/' + cartId + '/order').customPUT(paymentinformation);
+                });
+                return orderData;
+            });
+            return placeOrder;
+        }
+
+        $log.info('--- Cart service end ---');
+    }
 })();

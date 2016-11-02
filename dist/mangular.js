96dadf8bcc10415c5f8d4fba05fbedf561083a41
Oskar Thiel <oskar.thiel@vaimo.com>
2016-11-02 16:56:00 +0100

    Keep track of logged in customer in service

--- dist/mangular.js
+++ dist/mangular.js
@@ -155,20 +155,35 @@
   angular.module('mangular').service('Customer', Service);
   Service.$inject = [ 'Restangular' ];
   function Service(Restangular) {
+    var loggedInCustomer;
     var service = {
       login: login,
       logout: logout,
-      createAccount: createAccount
+      createAccount: createAccount,
+      loggedInCustomer: function() {
+        return loggedInCustomer;
+      }
     };
     return service;
     function login(user) {
-      return Restangular.one('/customers/login').customPOST(user);
+      return Restangular.one('/customers/login').customPOST(user).then(function(result) {
+        loggedInCustomer = result;
+        return result;
+      });
     }
     function logout(user) {
-      return Restangular.one('/customers/logout').customGET(user);
+      return Restangular.one('/customers/logout').customGET(user).then(function(result) {
+        loggedInCustomer = null;
+        return result;
+      });
     }
     function createAccount(user) {
-      return Restangular.one('/customers/create-account').customPOST(user);
+      return Restangular.one('/customers').customPOST({
+        customer: user
+      }).then(function(result) {
+        loggedInCustomer = result;
+        return result;
+      });
     }
   }
 })();
--- src/customer.service.js
+++ src/customer.service.js
@@ -10,24 +10,37 @@
     /* @ngInject */
     function Service(Restangular) {
 
+        var loggedInCustomer;
+
         var service = {
             login: login,
             logout: logout,
-            createAccount: createAccount
+            createAccount: createAccount,
+            loggedInCustomer: function(){return loggedInCustomer;}
         };
 
         return service;
 
         function login(user) {
-            return Restangular.one('/customers/login').customPOST(user);
+            return Restangular.one('/customers/login').customPOST(user).then(function (result){
+                loggedInCustomer = result;
+                return result;
+            });
         }
 
         function logout(user) {
-            return Restangular.one('/customers/logout').customGET(user);
+            return Restangular.one('/customers/logout').customGET(user).then(function (result){
+                loggedInCustomer = null;
+                return result;
+            });
         }
 
         function createAccount(user) {
-            return Restangular.one('/customers/create-account').customPOST(user);
+            return Restangular.one('/customers').customPOST({customer: user}).then(function (result){
+                loggedInCustomer = result;
+                return result;
+            });
         }
+
     }
 })();
